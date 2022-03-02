09c200b9e4b10538786b088d378ebe5efb92887d
Parth <parthkambli.pk1@gmail.com>
2022-03-02 19:54:03 +0530

    Home page

--- JS/Home.js
+++ JS/Home.js
@@ -8,7 +8,27 @@ function changeNav(){
 
 // Nav-on-Scrool
 
+var Nav = document.getElementById("NavBar");
+window.addEventListener('scroll', function(){
+    if (this.window.pageYOffset > 100){
+        Nav.classList.add('Nav-on-scroll');
+    }
+    else{
+        Nav.classList.remove('Nav-on-scroll');
+    }
+});
 
+// var mq = window.matchMedia("(max-width: 425px)");
+var Backgroun = document.getElementById("Bg-img");
+if ($(window).width()>=425) {
+    // window width is at less than 425px
+    Backgroun.classList.remove("vh-100");
+    Backgroun.style.height = "300px";
+}
+else {
+    // window width is greater than 425px
+    Backgroun.classList.add("vh-100")
+}
 
 // {/* <script type="text/javascript">
 //     var nav = document.querySelector('nav');
