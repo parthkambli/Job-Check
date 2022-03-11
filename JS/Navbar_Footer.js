d8c64fdd5bea2e23f8efd633ffe5a53bc4b67431
Parth <parthkambli.pk1@gmail.com>
2022-03-11 20:00:11 +0530

    stage 1

--- JS/Navbar_Footer.js
+++ JS/Navbar_Footer.js
@@ -3,8 +3,15 @@
 
 function changeNav() {
     var myNav = document.getElementById("NavBar");
+    var PostBtn = document.getElementById("Post-btn");
+    var SignupBtn = document.getElementById("Signup-btn")
     myNav.classList.toggle('Nav-on-toggle');
+    PostBtn.classList.toggle("Post-btn-position");
+    PostBtn.classList.toggle("my-2");
+    SignupBtn.classList.toggle("Signup-btn-position");
+    SignupBtn.classList.toggle("my-2");
 }
+
 
 // Nav-on-Scrool
 
